1a069a75f1b6dc6ecfdb1c7b4abc5322336362f1
mirandaflagg <mjf846s@missouristate.edu>
2024-02-11 17:36:09 -0600

    setOnClickListener commit

--- app/src/main/java/com/flagg/msu/trafficlightapp/MainActivity.kt
+++ app/src/main/java/com/flagg/msu/trafficlightapp/MainActivity.kt
@@ -4,17 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import com.flagg.msu.trafficlightapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val isRed =
+    private var visibility = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val isStop = true
+            val isGo = false
+            val isWait = false
+            if(isStop == true) {
+
+            } else if(isGo == true) {
+
+            } else {
+
+            }
+        }
 
     }
 }
