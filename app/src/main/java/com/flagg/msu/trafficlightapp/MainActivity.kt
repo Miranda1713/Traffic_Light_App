fc9e54b62e0b19fd527dc8ffcfa660247b765202
mirandaflagg <mjf846s@missouristate.edu>
2024-02-11 16:46:16 -0600

    drawables commit

--- app/src/main/java/com/flagg/msu/trafficlightapp/MainActivity.kt
+++ app/src/main/java/com/flagg/msu/trafficlightapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.flagg.msu.trafficlightapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.flagg.msu.trafficlightapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val isRed =
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
